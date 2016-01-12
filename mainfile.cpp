b03d9331409cbc8110ab6edbf21896d47b378493
Enacero <ghostdragon2013@gmail.com>
2016-01-12 23:22:49 +0200

    New main file

--- mainfile.cpp
+++ mainfile.cpp
@@ -1,6 +1,11 @@
-#include <stdlib.h>
-
-int main() {
-
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str1;
+    cin >> str1;
+    cout << str1 << endl;
+    cin.get();
     return 0;
 }
