c269ffc0edac534d31dcb4042ce55cdcc56dda92
Enacero <ghostdragon2013@gmail.com>
2016-01-13 00:14:43 +0200

    Delete test.cpp

--- test.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by paul on 12.01.16.
-//
-
-//test creation of file to test fork working, commiting and pull-requesting
-//please, delete before reading, I have to test remote deleting from my local fork. Thx
